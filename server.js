d069ff7bab97d06ff26b7148127c364de8c8548d
Erick Barros <erickbarrosrezende@gmail.com>
2024-05-22 20:26:36 -0300

    feat: create initial setup for the api

---
+++ server.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import app from "./App";
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log();
+  console.log(`> Server is listening on port ${port}`);
+});
