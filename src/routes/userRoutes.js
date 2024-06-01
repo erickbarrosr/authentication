ba79e24d57c9e597f08d55ec2ea3537f2106a947
Erick Barros <erickbarrosrezende@gmail.com>
2024-06-01 11:56:02 -0300

    feat(userRoutes.js): added routes to login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import LoginController from "../controllers/LoginController";
 
 const router = new Router();
 
 router.post("/register", UserController.registerUser);
+router.post("/login", LoginController.authenticateUser);
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.json({ message: "Logged out" });
+});
 
 export default router;
